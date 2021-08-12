0b9f057a22e4e6ae7b2dbbb898ea2ac5aa44d60e
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-08-12 19:11:13 +0530

    Create day1-exercise3.py

---
+++ day1-exercise3.py
@@ -0,0 +1,10 @@
+#print(Day 1-String Manipulation")
+print("Day 1-String Manipulation")
+
+print('e.g.print("Hello "+"World")')
+
+#print("String concatenation is done with the "+" sign.")
+print('String concatenation is done with the '' + '"+" + 'sign.')
+
+#print(("New lines can be created with a backslash and n")
+print("New lines can be created with a backslash and n")
