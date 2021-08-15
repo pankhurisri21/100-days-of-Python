6c26dbdef7c1c04d035d4f96a6ef886ce5127ac6
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-08-16 00:46:35 +0530

    Create type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,10 @@
+#int to str
+a=20
+print("Type of a :", type(a))
+print("New type of a :",type(str(a)))
+
+#float to int
+b=2.2
+print("Type of b :", type(b))
+print("New type of b :",type(int(b)))
+
