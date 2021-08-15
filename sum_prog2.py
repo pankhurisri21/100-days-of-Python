ae7da6a71bd1d6fe7404ec0f89d2fc1f419a7835
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-08-16 00:35:03 +0530

    Create sum_prog2.py

---
+++ sum_prog2.py
@@ -0,0 +1,4 @@
+#sum of n numbers 
+numbers=[1,2,3,4,5]
+print("Sum of numbers in the list = ", sum(numbers))#sum of numbers in the list
+print("Sum of numbers in the list and the start number =",sum(numbers,20))#sum ofnumbers in the list + the start number given
