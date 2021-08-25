c191a160c24b01aa5bc82b200dad4b3ce4b4d418
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-08-26 00:56:42 +0530

    Create list1.py

---
+++ list1.py
@@ -0,0 +1,15 @@
+#List declaration
+mylist=["palak","pankhuri"]
+print(mylist)
+print("length of mylist=",len(mylist))
+mylist.append("anurag")
+print("New list=",mylist)
+print("length of new mylist=",len(mylist))
+
+
+#using constructor
+
+thislist=list(("mango","banana","apple"))
+print(thislist)
+
+
