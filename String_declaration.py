31c484b49632a19e73430e91a46841158d69688d
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-08-26 00:35:25 +0530

    Create String_declaration.py

---
+++ String_declaration.py
@@ -0,0 +1,7 @@
+#You can use double or single quotes or three single quotes or three double quotes:
+print('This is single quoted string')
+print("This is double quoted string")
+print('''This is a 
+multiline 
+string''')
+
