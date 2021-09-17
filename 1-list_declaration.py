600511263fd890b2bb348aa82b9c0c8ea3ee4b02
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-09-18 00:23:24 +0530

    Create 1-list_declaration.py

---
+++ 1-list_declaration.py
@@ -0,0 +1,12 @@
+#List declaration
+
+#List with similar type of values
+
+mycarlist=["Ritz","Alto","Swift"]
+print(mycarlist)
+
+#List with mulitple type of values
+
+mylist=["item",20,34.98,True]
+print(mylist)
+
