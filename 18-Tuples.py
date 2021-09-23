1f8466485799fc5ee04f2145530b9dac74be51e1
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-09-24 00:32:19 +0530

    Update 18-Tuples.py

--- 18-Tuples.py
+++ 18-Tuples.py
@@ -1,9 +1,9 @@
 #Tuple
-mylist1=("Delhi","Mumbai","Pune","Bangalore")
-print(mylist1) #('Delhi', 'Mumbai', 'Pune', 'Bangalore')
+mytuple1=("Delhi","Mumbai","Pune","Bangalore")
+print(mytuple1) #('Delhi', 'Mumbai', 'Pune', 'Bangalore')
 
 
 #Tuples can have duplicate values
-mylist2=("Delhi","Mumbai","Pune","Pune","Delhi","Bangalore")
-print(mylist2)
+mytuple2=("Delhi","Mumbai","Pune","Pune","Delhi","Bangalore")
+print(mytuple2)
 
