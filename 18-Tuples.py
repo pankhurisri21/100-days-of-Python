b08b40896bb982d77f014a6c3dab61c7387f24fa
pankhurisri21 <88797608+pankhurisri21@users.noreply.github.com>
2021-09-24 00:28:35 +0530

    Create 18-Tuples.py

---
+++ 18-Tuples.py
@@ -0,0 +1,9 @@
+#Tuple
+mylist1=("Delhi","Mumbai","Pune","Bangalore")
+print(mylist1) #('Delhi', 'Mumbai', 'Pune', 'Bangalore')
+
+
+#Tuples can have duplicate values
+mylist2=("Delhi","Mumbai","Pune","Pune","Delhi","Bangalore")
+print(mylist2)
+
